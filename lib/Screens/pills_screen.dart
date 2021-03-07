47cd47f4651fffe0ee9671c8e77fe4c4707d2db2
Taha <tahanaeem12@hotmail.com>
2021-03-06 23:44:52 -0500

    new changes

--- lib/Screens/pills_screen.dart
+++ lib/Screens/pills_screen.dart
@@ -1,7 +1,10 @@
-import 'package:deltahacks/pill.dart';
+import 'package:deltahacks/components/popup.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:deltahacks/components/pill.dart';
+import 'package:deltahacks/components/pillTile.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 const data = {
   {"name": "pill1", "Pills Left": 7, "Schedule": "1,0,1,2,0,1,0"},
@@ -22,97 +25,29 @@ class _PillsState extends State<PillsPage> {
           itemBuilder: (BuildContext context, int index) {
             return PillTile(index);
           }),
+      floatingActionButton: Padding(
+          padding: EdgeInsets.only(bottom: 100),
+          child: FloatingActionButton.extended(
+            onPressed: () {
+              return AddViewPopup().popup(context);
+            },
+            label: Icon(Icons.add),
+          )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
 }
 
-class PillTile extends StatefulWidget {
-  final int index;
-  PillTile(this.index);
-  @override
-  _PillTileState createState() => _PillTileState();
-}
-
-class _PillTileState extends State<PillTile> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(border: Border.all()),
-        margin: EdgeInsets.all(10),
-        padding: const EdgeInsets.all(10),
-        child: ListTile(
-          title: Text(
-            listOfPills[widget.index].name,
-          ),
-          trailing: GestureDetector(
-            onTap: () => EditViewPopup(title: listOfPills[widget.index].name)
-                .popup(context),
-            child: Icon(Icons.create_rounded),
-          ),
-        ));
-  }
-}
-
-class EditViewPopup {
-  final String title;
-  int numberOfPills, pillPerDay;
-
-  EditViewPopup({this.title});
-  popup(context) {
-    return AwesomeDialog(
-      context: context,
-      title: title,
-      headerAnimationLoop: false,
-      dialogType: DialogType.INFO,
-      animType: AnimType.SCALE,
-      body: Padding(
-          padding: EdgeInsets.all(2),
-          child: Column(
-            children: [
-              Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Text(
-                    title,
-                    style: TextStyle(
-                        color: Color.fromRGBO(0, 0, 0, 1), fontSize: 30),
-                  )),
-              Padding(
-                  padding: EdgeInsets.all(10),
-                  child: TextField(
-                    obscureText: false,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), labelText: "Total Pills"),
-                  )),
-              Padding(
-                  padding: EdgeInsets.all(10),
-                  child: TextField(
-                    obscureText: false,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Pills per Day"),
-                  )),
-              Padding(
-                padding: EdgeInsets.all(2),
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return PillsPage();
-                      }));
-                    },
-                    child: Text("Save")),
-              )
-            ],
-          )),
-    )..show();
-  }
-}
-
-// IconButton(
-//             icon: Icon(Icons.create_rounded),
-//             onPressed: () {
-//               setState(() {
-//                 print("Pressed!");
-//               });
-//             },
-//           )
+// bottomNavigationBar: CurvedNavigationBar(
+//         backgroundColor: Colors.blueAccent,
+//         items: <Widget>[
+//           Icon(Icons.analytics),
+//           Icon(Icons.home),
+//           Icon(Icons.medical_services_rounded)
+//         ],
+//         onTap: (index) {
+//           setState(() {
+//             _page = index;
+//           });
+//         },
+//       )
---
+++ lib/components/pillTile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'pill.dart';
+import 'popup.dart';
+
+class PillTile extends StatefulWidget {
+  final int index;
+  PillTile(this.index);
+  @override
+  _PillTileState createState() => _PillTileState();
+}
+
+class _PillTileState extends State<PillTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(border: Border.all()),
+        margin: EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
+        child: ListTile(
+          title: Text(
+            listOfPills[widget.index].name,
+          ),
+          trailing: GestureDetector(
+            onTap: () => EditViewPopup(title: listOfPills[widget.index].name)
+                .popup(context),
+            child: Icon(Icons.create_rounded),
+          ),
+        ));
+  }
+}
